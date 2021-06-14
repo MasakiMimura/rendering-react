f748ea25709ecfc6ad3d17c04c608c7ddd0ccd6e
Masaki Mimura <62504066+MasakiMimura@users.noreply.github.com>
2021-06-15 08:30:25 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import "./styles.css";
+
+export default function App() {
+  console.log("App");
+  const [count, setCount] = useState(0);
+
+  const onClickCountUp = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <div className="App">
+      <p>{count}</p>
+      <button onClick={onClickCountUp}>カウントアップ</button>
+    </div>
+  );
+}
